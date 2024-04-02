e3bfe297e44c81ec279190a817bdfe933aa05220
Taylor Chiles <chilestaylor@gmail.com>
2024-04-02 14:37:08 -0700

    added auth

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import './App.css';
+import '@aws-amplify/ui-react/styles.css';
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  View,
+  Card,
+} from '@aws-amplify/ui-react';
 
-function App() {
+function App({ signOut }) {
   return (
-    <>
-      <main>
-        <h2>testing</h2>
-      </main>
-    </>
+    <View className='App'>
+      <Card>
+        <Heading level={1}>We now have Auth!</Heading>
+      </Card>
+      <Button onClick={signOut}>Sign Out</Button>
+    </View>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
